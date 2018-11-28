7cefb74451acbdecfc367b4682dc0f26b019f968
Nicholas <npere017@ucr.edu>
2018-11-28 11:16:03 +0000

    All seats (except drivers: on purpose) can be disabled/enabled, with a photoshopped picture difference

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController:UIViewController {
     
+    
+    @IBOutlet weak var Driver_top_left_row_1: UIImageView!
+    @IBOutlet weak var Passenger_left_row_2: UIImageView!
+    @IBOutlet weak var Passenger_left_row_3: UIImageView!
+    @IBOutlet weak var Passenger_right_row_1: UIImageView!
+    @IBOutlet weak var Passenger_right_row_2: UIImageView!
+    @IBOutlet weak var Passenger_right_row_3: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,6 +27,73 @@ class ViewController:UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    //MARK: Actions
+    @IBAction func EnableDisable_right_r1(_ sender: UITapGestureRecognizer)
+    {
+        if(Passenger_right_row_1.image == UIImage(named: "Disabled_seat"))
+        {
+            Passenger_right_row_1.image = UIImage(named: "Seats")
+        }
+        else
+        {
+            Passenger_right_row_1.image = UIImage(named: "Disabled_seat")
+        }
+    }
+
+    
+    @IBAction func EnableDisable_left_r2(_ sender: UITapGestureRecognizer)
+    {
+        if(Passenger_left_row_2.image == UIImage(named: "Disabled_seat"))
+        {
+            Passenger_left_row_2.image = UIImage(named: "Seats")
+        }
+        else
+        {
+            Passenger_left_row_2.image = UIImage(named: "Disabled_seat")
+        }
+    }
+    
+    @IBAction func EnableDisable_right_r2(_ sender: UITapGestureRecognizer)
+    {
+        if(Passenger_right_row_2.image == UIImage(named: "Disabled_seat"))
+        {
+            Passenger_right_row_2.image = UIImage(named: "Seats")
+        }
+        else
+        {
+            Passenger_right_row_2.image = UIImage(named: "Disabled_seat")
+        }
+    }
+    
+    @IBAction func EnableDisable_left_r3(_ sender: UITapGestureRecognizer)
+    {
+        if(Passenger_left_row_3.image == UIImage(named: "Disabled_seat"))
+        {
+            Passenger_left_row_3.image = UIImage(named: "Seats")
+        }
+        else
+        {
+            Passenger_left_row_3.image = UIImage(named: "Disabled_seat")
+        }
+    }
+    
+    @IBAction func EnableDisable_right_r3(_ sender: UITapGestureRecognizer)
+    {
+        if(Passenger_right_row_3.image == UIImage(named: "Disabled_seat"))
+        {
+            Passenger_right_row_3.image = UIImage(named: "Seats")
+        }
+        else
+        {
+            Passenger_right_row_3.image = UIImage(named: "Disabled_seat")
+        }
+    }
+    
+    
+    
+    
+    
     
     
     
