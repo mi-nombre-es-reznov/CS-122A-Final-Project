81f92aa88ae868884c41e70fc5c4f0e9eca26269
Nicholas <npere017@ucr.edu>
2018-11-28 09:41:32 +0000

    Redoing the swift code for the app. Still trying to learn how to operate it. The layout is done for the app loading screen and the base model

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Seatbelt_Savior
+//
+//  Created by Nicholas Perez-Aguilar on 11/20/18.
+//  Copyright © 2018 Nicholas Perez-Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    
+}
